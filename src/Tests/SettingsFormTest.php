ec0bd2cdd2a9dc7516c50a4549967ef9de8701b3
Antonin Slejska <slejska@schluetersche.de>
2017-05-03 16:17:19 +0200

    Adjusts installed fields

--- src/Tests/SettingsFormTest.php
+++ src/Tests/SettingsFormTest.php
@@ -18,15 +18,6 @@ class SettingsFormTest extends WebTestBase {
    * @var array
    */
   public static $modules = array(
-    'file',
-    'field',
-    'text',
-    'filter',
-    'user',
-    'system',
-    'views',
-    'image',
-    'token',
     'file_entity',
     'test_module',
   );
